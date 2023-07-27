cc98c7f701199b283b2aee21993e71cf3455b473
Nonato Sousa <nonato.sousa@antikytera.com>
2023-07-27 15:25:44 -0300

    * sample project

---
+++ Assets/SaveMedia.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+public static class SaveMedia
+{
+    public delegate void OnSuccess(string value);
+    public delegate void OnError(string value);
+
+    [DllImport("__Internal")]
+    private static extern void saveImage(string imageNameWithExtension, string albumName, OnSuccess onSuccess, OnError onError);
+
+    [DllImport("__Internal")]
+    private static extern void saveVideo(string videoNameWithExtension, string albumName, OnSuccess onSuccess, OnError onError);
+
+    public static void SaveImage(string imageNameWithExtension, string albumName, OnSuccess onSuccess, OnError onError)
+    {
+        saveImage(imageNameWithExtension: imageNameWithExtension, albumName: albumName, onSuccess: onSuccess, onError: onError);
+    }
+
+    public static void SaveVideo(string videoNameWithExtension, string albumName, OnSuccess onSuccess, OnError onError)
+    {
+        saveVideo(videoNameWithExtension: videoNameWithExtension, albumName: albumName, onSuccess: onSuccess, onError: onError);
+    }
+
+}
+
---
+++ Assets/SaveMediaBehaviour.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.IO;
+using System.Runtime.InteropServices;
+using AOT;
+using UnityEngine;
+using UnityEngine.Networking;
+using static SaveMedia;
+
+public class SaveMediaBehaviour : MonoBehaviour
+{
+    [SerializeField] string _imageName = "image.png";
+    [SerializeField] string _videoName = "video.mp4";
+    [SerializeField] string _albumName;
+    [SerializeField] Texture2D _texture;
+    [SerializeField] string _video;
+
+    public void WriteMedia()
+    {
+        string path = Path.Combine(Application.persistentDataPath, _imageName);
+        File.WriteAllBytes(path, _texture.EncodeToPNG());
+
+        Debug.Log($"Image saved at: {path}");
+        StartCoroutine(GetVideo());
+    }
+
+    public void SaveImage()
+    {
+       SaveMedia.SaveImage(imageNameWithExtension: _imageName, _albumName, onSuccess: OnImageSavedSuccess, onError: OnImageSaveError);
+    }
+
+    [MonoPInvokeCallback(typeof(OnSuccess))]
+    private static void OnImageSavedSuccess(string message)
+    {
+        Debug.Log($"[UNITY] image saved: {message}");
+    }
+
+    [MonoPInvokeCallback(typeof(OnError))]
+    private static void OnImageSaveError(string message)
+    {
+        Debug.Log($"[UNITY] image not saved: {message}");
+    }
+
+    public void SaveVideo()
+    {
+        SaveMedia.SaveVideo(_videoName, _albumName, onSuccess: OnVideoSavedSuccess, onError: OnVideoSaveError);
+    }
+
+    [MonoPInvokeCallback(typeof(OnSuccess))]
+    private static void OnVideoSavedSuccess(string message)
+    {
+        Debug.Log($"[UNITY] Video saved: {message}");
+    }
+
+    [MonoPInvokeCallback(typeof(OnError))]
+    private static void OnVideoSaveError(string message)
+    {
+        Debug.Log($"[UNITY] Video not saved: {message}");
+    }
+
+    IEnumerator GetVideo()
+    {
+        var request = UnityWebRequest.Get(_video);
+
+        yield return request.SendWebRequest();
+
+        var data = request.downloadHandler.data;
+
+        var path = Path.Combine(Application.persistentDataPath, "video.mp4");
+
+        File.WriteAllBytes(path, data);
+        Debug.Log($"Video saved at: {path}");
+    }
+
+}
